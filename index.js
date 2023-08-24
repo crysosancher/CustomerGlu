e9d99724cb4ef959e83e0815301e773c0de2d8ab
vibhu pandey <77499291+crysosancher@users.noreply.github.com>
2023-08-24 17:26:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,80 @@
 const TelegramBot = require('node-telegram-bot-api');
-const os = require('os');
-const ping = require('ping');
+const ps = require('ps-node');
+const axios = require('axios');
 
-const bot = new TelegramBot('YOUR_BOT_TOKEN', { polling: true });
+// Set up your Telegram Bot API token
+const TELEGRAM_TOKEN = "TeleGram Token";
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
-// Periodically send server health stats to users
-setInterval(sendHealthStats, 6 * 60 * 60 * 1000); // Every 6 hours
-
-// Handle /stats command to send current health stats
-bot.onText(/\/stats/, async (msg) => {
-  const chatId = msg.chat.id;
-  const stats = await getHealthStats();
-  bot.sendMessage(chatId, stats);
-});
-
-// Handle /processes command to send list of running processes
-bot.onText(/\/processes/, async (msg) => {
-  const chatId = msg.chat.id;
-  const processes = await getRunningProcesses();
-  bot.sendMessage(chatId, processes);
-});
-
-// Function to send health stats to users
-async function sendHealthStats() {
-  const stats = await getHealthStats();
-  bot.sendMessage(chatId, stats);
-}
-
-// Function to gather server health stats
-async function getHealthStats() {
-  const totalMemory = os.totalmem();
-  const freeMemory = os.freemem();
-  const cpuUsage = await getCpuUsage();
-  
-  return `CPU Usage: ${cpuUsage}%, Free Memory: ${freeMemory} bytes, Total Memory: ${totalMemory} bytes`;
-}
-
-// Function to get CPU usage percentage
-async function getCpuUsage() {
-  // Implement logic to calculate CPU usage
-  // This could involve reading from /proc/stat or using third-party libraries
-  // For simplicity, we'll return a random number between 0 and 100 here.
-  return Math.floor(Math.random() * 101);
-}
-
-// Function to get list of running processes
-async function getRunningProcesses() {
-  // Implement logic to retrieve running processes
-  // For simplicity, we'll return a mock list of processes.
-  const processes = ['Process 1', 'Process 2', 'Process 3'];
-  return processes.join('\n');
-}
-
-// Function to check VM responsiveness and notify users
-setInterval(checkVMResponsiveness, 5000); // Every 5 seconds
-
-async function checkVMResponsiveness() {
-  // Implement logic to ping the VM and check responsiveness
-  // For simplicity, we'll use the 'ping' library.
-  const vmIp = 'VM_IP_ADDRESS';
-  const res = await ping.promise.probe(vmIp);
-  
-  if (!res.alive) {
-    const users = ['USER_CHAT_ID_1', 'USER_CHAT_ID_2']; // Add user chat IDs
-    users.forEach(chatId => {
-      bot.sendMessage(chatId, `VM at ${vmIp} is unresponsive!`);
-    });
+// Function to get server health stats
+function getHealthStats() {
+  try {
+    const cpuPercent = Math.round(require('os').loadavg()[0] * 100);
+    const ramPercent = Math.round((1 - require('os').freemem() / require('os').totalmem()) * 100);
+    return `CPU: ${cpuPercent}%\nRAM: ${ramPercent}%`;
+  } catch (error) {
+    console.error('Error getting health stats:', error);
+    return 'Error getting health stats.';
   }
 }
+
+// Command handler for '/stats' command
+bot.onText(/\/stats/, (msg) => {
+  const chatId = msg.chat.id;
+  const stats = getHealthStats();
+  bot.sendMessage(chatId, stats);
+});
+
+// Function to check VM responsiveness
+async function checkVmResponsiveness() {
+  const vmUrl = 'http://your_vm_url';
+  try {
+    await axios.get(vmUrl, { timeout: 5000 });
+  } catch (error) {
+    notifyUsersVmNotResponding();
+  }
+}
+
+// Function to notify users if VM is not responding
+function notifyUsersVmNotResponding() {
+  // Implement sending notifications to users
+}
+
+// Command handler for '/processes' command
+bot.onText(/\/processes/, (msg) => {
+  const chatId = msg.chat.id;
+  try {
+    ps.lookup({}, (err, processes) => {
+      if (err) {
+        console.error('Error getting processes:', err);
+        bot.sendMessage(chatId, 'Error getting processes.');
+        return;
+      }
+      
+      const sortedProcesses = processes.sort((a, b) => b.cpu - a.cpu); // Sort by CPU usage
+      const topProcesses = sortedProcesses.slice(0, 10); // Get top 10 processes
+      
+      const processesList = topProcesses.map(proc => `PID: ${proc.pid}, Name: ${proc.command}`).join('\n');
+      bot.sendMessage(chatId, processesList);
+    });
+  } catch (error) {
+    console.error('Error handling /processes command:', error);
+    bot.sendMessage(chatId, 'Error processing /processes command.');
+  }
+});
+
+// Periodically check VM responsiveness
+setInterval(() => {
+  try {
+    checkVmResponsiveness();
+  } catch (error) {
+    console.error('Error checking VM responsiveness:', error);
+  }
+}, 5000);
+
+// Handle other messages and commands
+bot.on('message', (msg) => {
+  // Handle other messages if needed
+});
+
+console.log('Bot is running...');
